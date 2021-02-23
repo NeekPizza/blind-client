ca07197accf71ba0440559b2a585f3f0d7740892
Nick Piazza <nick@onlinemeded.org>
2021-02-23 00:46:49 -0600

    Install Prettier ticket #17

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 const ENDPOINT = 'http://127.0.0.1:4001';
 
@@ -9,37 +9,37 @@ function App() {
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     console.log(socket);
-    socket.on('hello', data => {
+    socket.on('hello', (data) => {
       setResponse(data);
     });
 
     return () => socket.disconnect();
   }, []);
 
-  const submitName = event => {
+  const submitName = (event) => {
     event.preventDefault();
-   setPlayerName(event.target[0].value);
-  }
+    setPlayerName(event.target[0].value);
+  };
 
   return (
     <React.Fragment>
       <p>server response: {response}</p>
       <p>name: {playerName || 'None submitted yet'}</p>
-      {!playerName &&
-       <div>
+      {!playerName && (
+        <div>
           <form onSubmit={submitName}>
-          <div>
-            <label htmlFor="name">Enter your name: </label>
-            <input id="name" type="text" name="name" />
-          </div>
-          <div>
-            <input type="submit" value="Send" />
-          </div>
-        </form>
-       </div>
-      }
+            <div>
+              <label htmlFor="name">Enter your name: </label>
+              <input id="name" type="text" name="name" />
+            </div>
+            <div>
+              <input type="submit" value="Send" />
+            </div>
+          </form>
+        </div>
+      )}
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
