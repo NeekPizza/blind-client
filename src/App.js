f90cf5dd8f9a507dfaba603e344c4eaa0905bd63
Nick Piazza <nick@onlinemeded.org>
2021-02-23 00:09:28 -0600

    accomplish ticket 15: https://b24-ep3sp0.bitrix24.com/workgroups/group/9/tasks/task/view/15/

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const ENDPOINT = 'http://127.0.0.1:4001';
 
 function App() {
   const [response, setResponse] = useState('');
+  const [playerName, setPlayerName] = useState(null);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
@@ -15,11 +16,30 @@ function App() {
     return () => socket.disconnect();
   }, []);
 
+  const submitName = event => {
+    event.preventDefault();
+   setPlayerName(event.target[0].value);
+  }
+
   return (
-    <p>
-      {response}
-    </p>
-  );
+    <React.Fragment>
+      <p>server response: {response}</p>
+      <p>name: {playerName || 'None submitted yet'}</p>
+      {!playerName &&
+       <div>
+          <form onSubmit={submitName}>
+          <div>
+            <label htmlFor="name">Enter your name: </label>
+            <input id="name" type="text" name="name" />
+          </div>
+          <div>
+            <input type="submit" value="Send" />
+          </div>
+        </form>
+       </div>
+      }
+    </React.Fragment>
+  )
 }
 
 export default App;
